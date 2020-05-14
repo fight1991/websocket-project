eaa3e7a8537d884528e3dcc543dc6fc26bda2324
fight1991 <359193813@qq.com>
2020-05-14 14:18:41 +0800

    上传文件

--- static/update.js
+++ static/update.js
@@ -1,14 +1,15 @@
 $(function() {
   layui.use(['layer', 'element', 'upload', 'form'], function(){
     var layer = layui.layer, form = layui.form, element = layui.element, upload = layui.upload;
-    var reader = null; // 读取器
+    var reader = new FileReader(); // 读取器
     var step = 2; // 每次读取字节数b
     var fileObj = null; // 文件对象
     var cuLoaded = 0; // 当前已经读取的字节总数
     var totalLoaded = 0; // 当前文件总字节数
     var ws = null;
+    var sum = 0; // 和校验, 取末尾2位数
     var isConnect = false; // 是否成功建立连接
-    createSocket(); // 建立websocket
+    // createSocket(); // 建立websocket
     form.verify({
       testaa: function (value, item) {
         if (!value) {
@@ -33,9 +34,15 @@ $(function() {
         layer.msg('请选择文件', {icon: 2});
         return false
       }
+      // 先发送包文件信息
+      let headInfo = {
+        fun: 'up', // up 升级 set 配网
+        size: totalLoaded,
+        sum: sum
+      }
+      ws.send(JSON.stringify(headInfo))
       // 分段读取开始
-      reader = new FileReader()
-      readBlob()
+      // readBlob()
       return false; // 阻止表单提交
     });
     //1.取上传的文件对象
@@ -54,6 +61,12 @@ $(function() {
           totalLoaded = file.size
           fileObj = file
           $('.filename').text(file.name)
+          reader.readAsArrayBuffer(fileObj)
+          reader.onload = function () {
+            console.log(this.result)
+            let unit8 = new Uint8Array(this.result)
+            sum = sumCheck(unit8, 2)
+          }
         })
       }
     });
@@ -62,7 +75,7 @@ $(function() {
     // });
     function readBlob () {
       if (cuLoaded > totalLoaded) {
-        console.log('文件传输完毕')
+        layer.msg('文件传输完毕', {icon: 1})
         return
       }
       let temp = fileObj.slice(cuLoaded, cuLoaded + step)
@@ -75,8 +88,12 @@ $(function() {
           percent = 100
         }
         element.progress('upprogress', percent + '%')
-        ws.send(this.result)
-      } 
+        // 帧头2 + 命令1 + 数据长度2 + 索引1 + 数据域n + 帧尾2
+        // var tempBuffer8 = new Int8Array(this.result)
+        // var totalLength = 2 + 1 + 2 + 1 + tempBuffer8.length + 2
+        // let result = mergeBuffer(totalLength, cuLoaded / step, tempBuffer8).buffer
+        ws.send(result)
+      }
     }
     function createSocket(success) {
       var index = layer.load();
@@ -103,5 +120,32 @@ $(function() {
         layer.close(index);
       }
     }
+    function mergeBuffer(totalLength, index, newBuffer) {
+      let arry = new Int8Array(totalLength)
+      // 添加帧头
+      arry[0] = '0xA'
+      arry[1] = 'EBF'
+      // 添加命令
+      arry[2] = '0x21'
+      // 添加数据长度
+      arry[3] = '0x00'
+      arry[4] = '0x05'
+      // 添加数据索引
+      arry[5] = index
+      newBuffer.forEach((v, i) => {
+        arry[5 + i + 1] = v
+      })
+      // 0xECFD
+      arry[arry.length - 1] = 'CFD'
+      arry[arry.length - 2] = '0xE'
+      return arry
+    }
+    function sumCheck (arr, length) {
+      let total = 0
+      for (let i = 0; i < arr.length; i++) {
+        total += arr[i]
+      }
+      return Number(total.toString().slice(arr.length-length))
+    } 
   });
 })
