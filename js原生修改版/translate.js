5c897250e99aa1b921733ce5ca37c8caf6a6ca80
fight1991 <359193813@qq.com>
2020-08-13 16:15:53 +0800

    中英文页面合并

---
+++ js原生修改版/translate.js
@@ -0,0 +1,66 @@
+let lang = {
+  zh: {
+    // dom字段开始
+    dom_connect: '网络连接中 . . .',
+    dom_setNet: '配网',
+    dom_empty: '无',
+    dom_pw: '密码',
+    dom_save: '保存',
+    dom_refresh: '刷新',
+    dom_clear1: '重置',
+    dom_gradeT: '本地升级',
+    dom_solf: '软件版本',
+    dom_firm: '硬件版本',
+    dom_selectF: '选择文件',
+    dom_selectP: '请选择',
+    dom_selectT: '只能上传.bin文件',
+    dom_gradeB: '升级',
+    dom_clear2: '重置',
+    // dom字段结束
+    // tips字段开始
+    txt_conTxt: '网络未连接！请重新连接',
+    txt_fail: '失败',
+    txt_success: '成功',
+    txt_timeout: '超时',
+    txt_ssId: '请选择SSID!',
+    txt_pw: '请输入密码!',
+    txt_ft: '文件类型错误, 请重新选择!',
+    txt_sz: '文件大小不能超过76K!',
+    txt_br: '请选择Chrome,Edge,Safari浏览器，避免未知错误!',
+    txt_file: '请选择文件!',
+    txt_init: '初始化页面...',
+    txt_wait: '升级中... 请等待 '
+  },
+  en: {
+    // dom字段开始
+    dom_connect: 'Connecting . . .',
+    dom_setNet: 'Set-up net',
+    dom_empty: 'nothing',
+    dom_pw: 'Password',
+    dom_save: 'Save',
+    dom_refresh: 'Refresh',
+    dom_clear1: 'Clear',
+    dom_gradeT: 'Local upgrade',
+    dom_solf: 'Software version',
+    dom_firm: 'Hardware version',
+    dom_selectF: 'Select File',
+    dom_selectP: 'Please select',
+    dom_selectT: 'Only .bin files can be uploaded',
+    dom_gradeB: 'Upgrade',
+    dom_clear2: 'Clear',
+    // dom字段结束
+    // tips字段开始
+    txt_conTxt: 'The network is not connected ! Please reconnect',
+    txt_fail: 'Failure',
+    txt_success: 'Successful',
+    txt_timeout: 'Time out!',
+    txt_ssId: 'Please select a ssId !',
+    txt_pw: 'Please enter your password !',
+    txt_ft: 'The type of file is invalid, please select again !',
+    txt_sz: 'The file cannot be greater than 76K, please select again !',
+    txt_br: 'Please use Chrome,Edge or Safari to avoid unknown errors !',
+    txt_file: 'Please select file!',
+    txt_init: 'Initializing the page...',
+    txt_wait: 'Updating... Please wait'
+  }
+}
